e2257a10977d5f813d363febffb50f59d25dc39a
Mohamed Gouda <mggouda@gmail.com>
2025-09-02 14:49:32 +0300

    composer installed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mohamedgouda/ecatalogue-for-woocommerce',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd15bc5c27db7a8787e3018e6661177b6fb61e3ee',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mohamedgouda/ecatalogue-for-woocommerce' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd15bc5c27db7a8787e3018e6661177b6fb61e3ee',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tecnickcom/tcpdf' => array(
+            'pretty_version' => '6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'ca5b6de294512145db96bcbc94e61696599c391d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
